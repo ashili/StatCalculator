9bd5f2c8d2d631059992f3bddaa4f72c24e8b4a7
anis <anisshili.as@gmail.com>
2020-07-05 10:32:16 -0400

    Feature : Added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Calculator',
+      version='0.0.1',
+      description='Calculator',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(),
+      )
