441f331508c94ca5e561262025efecf0377f3684
Maksim <MaksimNizh@gmail.com>
2020-07-05 11:44:05 -0400

    Added methods simpleRandomSample, confidenceInterval, marginOfError, cochranSampleSize, and sampleSize to StatsCalculator.py

--- Statistics/StatCalculator.py
+++ Statistics/StatCalculator.py
@@ -1,5 +1,6 @@
 import Calculator
 import math
+import random
 
 
 def isEven(num):
@@ -52,3 +53,31 @@ class StatCalc(Calculator):
     def zScore(self, num, nums):
         z = (float(num) - self.mean(nums)) / self.stdDev(nums)
         return z
+
+    def simpleRandomSample(self, num, nums):
+        return random.sample(nums, num)
+
+    def confidenceInterval(self, nums):
+        length = len(nums)
+        numsMean = self.mean(nums)
+        stanDev = self.stdDev(nums)
+        lowerBound = numsMean + 1.96 * (stanDev / math.sqrt(length))
+        upperBound = numsMean - 1.96 * (stanDev / math.sqrt(length))
+        returnResult = [lowerBound, upperBound]
+        return returnResult
+
+    def marginOfError(self, nums):
+        length = len(nums)
+        stanDev = self.stdDev(nums)
+        stanError = stanDev/math.sqrt(length)
+        margin = stanError * 1.96
+        return margin
+
+    def cochranSampleSize(self, marginOfError, estimatedProportion):
+        return ((1.96 ^ 2) * estimatedProportion * (1 - estimatedProportion)) / (marginOfError ^ 2)
+
+    def sampleSize(self, confidenceInterval, width):
+        marginOfError = width / 2
+        pHat = .5
+        qHat = 1 - pHat
+        return (pHat * qHat) * ((1.96 / marginOfError) ^ 2)
