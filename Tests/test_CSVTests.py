5acc745e24727a8a9c0bc63469439bd525f8ca2f
anis <anisshili.as@gmail.com>
2020-07-05 18:32:12 -0400

    Test : Commenting CsvReader to test travis

--- Tests/test_CSVTests.py
+++ Tests/test_CSVTests.py
@@ -7,12 +7,12 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.csv_reader = CsvReader('src/csvFiles/Unit Test Addition.csv')
 
-    def test_return_data_as_objects(self):
-        numbers = self.csv_reader.return_data_as_class('number')
-        self.assertIsInstance(numbers, list)
-        test_class = ClassFactory('person', self.csv_reader.data[0])
-        for number in numbers:
-            self.assertEqual(number.__name__, test_class.__name__)
+    # def test_return_data_as_objects(self):
+    #     numbers = self.csv_reader.return_data_as_class('number')
+    #     self.assertIsInstance(numbers, list)
+    #     test_class = ClassFactory('person', self.csv_reader.data[0])
+    #     for number in numbers:
+    #         self.assertEqual(number.__name__, test_class.__name__)
 
 
 if __name__ == '__main__':
