af4f98256b4163befc991b69b5207d11d851bef2
anis <anisshili.as@gmail.com>
2020-07-04 21:59:53 -0400

    Feature : implemented median  and mode function

--- StatCalculator.py
+++ StatCalculator.py
@@ -5,7 +5,32 @@ def mean(nums):
     return sum(nums) / nums
 
 
+def isEven(num):
+    if (num % 2) == 0:
+        return True
 
+
+def median(nums):
+    if isEven(len(nums) + 1):
+        r = nums[len(nums) / 2]
+    else:
+        r = (nums[len(nums) / 2] + nums[(len(nums) / 2) - 1]) / 2.00
+    return float(r)
+
+
+def mode(nums):
+    if len(nums) == 0:
+        return 0
+
+    d = dict()
+    for item in nums:
+        if item in d:
+            d[item] += 1
+        else:
+            d[item] = 1
+
+    f = max(d, key=d.get)
+    return f
 
 
 class StatCalc(Calculator):
